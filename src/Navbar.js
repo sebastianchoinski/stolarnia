abd1a13debf5a3af81afc32feca8bd0005c61f5d
Sebastian Choiński <sebastianchoinski6@gmail.com>
2024-01-07 17:05:23 +0100

    x

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,17 +2,17 @@ import React from 'react'
 import logo from './logo.png'
 import './Navbar.scss'
 import phoneicon from './phoneicon.png'
+import { Link } from "react-router-dom";
 export default function Navbar() {
     return(
         <div className='mainnavbardiv'>
             <img className='logo' src={logo}></img>
             <div className='wycena'><p>WYCENA W 24H</p></div>
             <ul className="navlinks">
-                <li className='navlink'> <a href='/'>Strona główna</a> </li>
-                <li className='navlink'> <a href='/projects'>Projekty</a> </li>
-                <li className='navlink'> <a href='/about'>O nas</a> </li>
-                <li className='navlink'> <a href='/contact'>Kontakt</a> </li>
-                <li className='navlink'> <a href='/blog'>Blog</a> </li>
+            <Link to="/"><li className='navlink'> <a>Strona główna</a> </li></Link>
+            <Link to="/projects"><li className='navlink'> <a>Projekty</a> </li></Link>
+            <Link to="/about"><li className='navlink'> <a>O nas</a> </li></Link>
+            <Link to="/contact"><li className='navlink'> <a>Kontakt</a> </li></Link>                                                    
             </ul>
             <div className='phonenumber'><img className="phoneicon" src={phoneicon}></img><a>+48 XXX XXX XXX</a></div>
         </div>
